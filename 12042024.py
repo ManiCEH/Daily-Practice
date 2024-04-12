a22ecf0fcda1821cac95a3b35ecd84d89463320f
maniceh <manieee.moorthy@gmail.com>
2024-04-12 11:51:21 +0530

    Initial commit

---
+++ 12042024.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from sklearn.preprocessing import MinMaxScaler 
+from sklearn.preprocessing import StandardScaler 
